ed94b3b335d7a8d655cfc123968abc530093cd48
joaolira-dev <joaovictorlira06@hotmail.com>
2025-06-13 10:39:36 -0300

    feat: add trending-item component

--- frontend/src/components/ui/trending-area.tsx
+++ frontend/src/components/ui/trending-area.tsx
@@ -1,9 +1,14 @@
+import { TrendingItem, TrendingItemSkeleton } from "./trending-item"
+
 export const TrendingArea = () => {
    return (
       <div className="bg-gray-700 rounded-3xl">
          <h2 className="text-xl p-6">O que está acontecendo?</h2>
-         <div className="flex flex-col gap-4 p-6">
-            ...
+         <div className="flex flex-col gap-4 p-6 pt-0">
+            <TrendingItem label={"#Teste"} count={2}/>
+            <TrendingItem label={"#Teste"} count={2}/>
+            <TrendingItemSkeleton/>
+            <TrendingItemSkeleton/>
          </div>
       </div>
    )
