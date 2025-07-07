c5526b880844aa14ca969e21b374889b01df96bf
joaolira-dev <joaovictorlira06@hotmail.com>
2025-07-07 11:59:12 -0300

    feat: adding header and post tweet in home page

---
+++ frontend/src/components/home/home-menu.tsx
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Logo from "../ui/logo";
+import { faHouse, faUser, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { SearchInput } from "../ui/search-input";
+import { NavItem } from "../ui/nav/nav-item";
+import { NavLogout } from "../ui/nav/nav-logout";
+
+type Props = {
+  closeAction: () => void;
+};
+export const HomeMenu = ({ closeAction }: Props) => {
+  return (
+    <div className="lg:hidden fixed inset-0 p-6 bg-black">
+      <div className="flex justify-between items-center">
+        <Logo size={32} />
+        <div
+          className="cursor-pointer flex justify-center items-center size-12 rounded-full border-2 border-gray-900"
+          onClick={closeAction}
+        >
+          <FontAwesomeIcon icon={faXmark} className="size-6" />
+        </div>
+      </div>
+
+      <div className="my-6">
+        <SearchInput />
+      </div>
+
+      <div>
+        <NavItem href="/home" icon={faHouse} label="Página inicial" />
+        <NavItem href="/profile" icon={faUser} label="Meu perfil" />
+        <NavLogout/>
+      </div>
+    </div>
+  );
+};
